f24af4907ab2dbe7d52942996160b5028e7f4c3c
Joaquín Jiménez García <garcia.jjimenez@gmail.com>
2018-10-09 18:20:32 +0200

    deshabilitada ruta para registro

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,7 +7,7 @@ var md_auth = require('../middlewares/authenticated')
 var multipart = require('connect-multiparty')
 var md_upload = multipart({uploadDir: './uploads/users'})
 
-api.post('/register', UserController.register)
+//api.post('/register', UserController.register)
 api.post('/login', UserController.login)
 //api.put('/update-user/:id',md_auth.ensureAuth, UserController.update)
 
